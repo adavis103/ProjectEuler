74f792bbed52d4cd2a265c434b928fdc860fd440
Alexander Davis <alexander.davis@saascg.com>
2022-06-10 22:23:48 -0500

    create SumEvenFibo method

--- ProjectEuler/MathHelper.cs
+++ ProjectEuler/MathHelper.cs
@@ -6,11 +6,32 @@ public class MathHelper
     {
         if (x > upperBound) return 0;
 
-        //multiples must be strictly less than upper bound
+        // Multiples must be strictly less than upper bound
         upperBound -= 1;
         
         // Use a summation to avoid looping
         int quotient = upperBound / x;
         return x * (quotient * (quotient + 1)) / 2;
+    }
+
+    public static int SumEvenFibonacci(int upperBound)
+    {
+        int sum = 0;
+        int prev2 = 0;
+        int prev = 1;
+
+        for (int i = 2; i < upperBound; i += prev2)
+        {
+            // Sum only even numbers in Fibonacci sequence
+            if (i % 2 == 0) 
+            {
+                sum += i;
+            }
+            
+            prev2 = prev;
+            prev = i;
+        }
+        
+        return sum;
     }
 }
