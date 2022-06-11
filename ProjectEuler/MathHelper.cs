4c2aa6c67bb2c29c14ce86770d89515ba558c3e2
Alexander Davis <alexander.davis@saascg.com>
2022-06-10 22:01:16 -0500

    create mathhelper class, summultiples method

---
+++ ProjectEuler/MathHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectEuler;
+
+public class MathHelper
+{
+    public static int SumMultiplesOfX(int x, int upperBound)
+    {
+        if (x > upperBound) return 0;
+
+        //multiples must be strictly less than upper bound
+        upperBound -= 1;
+        
+        // Use a summation to avoid looping
+        int quotient = upperBound / x;
+        return x * (quotient * (quotient + 1)) / 2;
+    }
+}
